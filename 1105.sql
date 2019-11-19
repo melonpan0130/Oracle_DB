dc65eb0894ba4dd08f5650c3142611f493e7493e
Eunju Chong <eun01300714@gmail.com>
2019-11-19 15:51:09 +0900

    1119

--- 1105.sql
+++ 1105.sql
@@ -8,3 +8,14 @@ WHERE salary > (SELECT MIN(salary)
     FROM employees natural join jobs
     where job_title like '%Sales%')
 ORDER BY salary;
+
+SELECT first_name, salary, department_id
+FROM employees
+WHERE salary > ANY (SELECT salary
+    FROM employees natural join jobs
+    where job_title like '%Sales%')
+ORDER BY salary;
+
+
+-- 다중 열 서브쿼리
+-- #.1
