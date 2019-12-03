fc9d77c1d12ba3b32c0b2bea60259564482c1240
Eunju/kiori <eun01300714@gmail.com>
2019-12-03 11:19:10 +0900

    1203

--- 1105.sql
+++ 1105.sql
@@ -23,12 +23,36 @@ ORDER BY 2;
 
 
 -- 다중 열 서브쿼리
-select first_name, manager_id, department_id
-from employees
-where first_name NOT IN ('Diana', 'Adam')
- AND (manager_id, department_id) IN 
- (select manager_id, department_id from employees where first_name in('Diana', 'Adam'));
+-- 다중 행으로 출력해보기
+SELECT first_name, manager_id, department_id
+FROM employees
+WHERE manager_id IN (SELECT manager_id FROM employees WHERE first_name IN ('Diana', 'Adam'))
+AND department_id IN (SELECT department_id FROM employees WHERE first_name IN ('Diana', 'Adam'))
+AND first_name NOT IN ('Diana', 'Adam');
 
+SELECT first_name, manager_id, department_id
+FROM employees
+WHERE first_name NOT IN ('Diana', 'Adam')
+AND (manager_id, department_id)
+    IN (SELECT manager_id, department_id FROM employees WHERE first_name in('Diana', 'Adam'));
+-- pairwise
 
 -- #.1
+select department_id, employee_id, first_name, salary
+from employees 
+where (department_id, salary) in (select department_id, min(salary) from employees) OR salary = (select max(salary) from employees);
+
+select department_id, min(salary), max(salary) from employees
+group by department_id;
+
+
+SELECT department_id, employee_id, first_name, salary
+FROM employees 
+WHERE (department_id, salary) IN (SELECT department_id, MIN(salary)
+        FROM employees
+        GROUP BY department_id)
+OR (department_id, salary) IN (SELECT department_id, max(salary) 
+        from employees
+        group by department_id)
+order by 1;
 
